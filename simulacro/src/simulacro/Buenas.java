1b5890fdfe9bcbb36a5f2f21682d68e1d42edd44
usuario <usuario@DESKTOP-5FAD0MS>
2022-11-29 20:48:00 +0100

    Archivo Buenas
    Se ha añadido un system.out.print con el mensaje de texto archivo buenas

---
+++ simulacro/src/simulacro/Buenas.java
@@ -0,0 +1,13 @@
+package simulacro;
+
+public class Buenas {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	System.out.println("Archivo Buenas");
+	
+	}
+}
